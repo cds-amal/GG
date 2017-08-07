3092d218a12c48d230da103e7ed6794aac206d42
Amal Sudama <cds.sudama@gmail.com>
2017-08-07 15:18:56 -0400

    check in wip

--- GuessingGameController.js
+++ GuessingGameController.js
@@ -1,5 +1,6 @@
 let game = new Game();
 
+// create a handler for the enterkey
 ko.bindingHandlers.enterkey = {
     init: function (element, valueAccessor, allBindings, viewModel) {
         var callback = valueAccessor();
@@ -17,11 +18,13 @@ ko.bindingHandlers.enterkey = {
 const blankGuesses = () => '-'.repeat(5).split('');
 
 function AppViewModel() {
+    // observables enables 2-way binding.
     this.title    = ko.observable(ResourceString[sPlay]);
     this.subtitle = ko.observable(ResourceString[sPrompt]);
     this.guess    = ko.observable("");
     this.guesses  = ko.observable(blankGuesses());
-    this.reset    = function() {
+
+    this.reset = function() {
       game = new Game();
       this.title(ResourceString[sPlay]);
       this.subtitle(ResourceString[sPrompt]);
@@ -29,26 +32,37 @@ function AppViewModel() {
       this.guess('');
     }
 
-    this.turn     = function() {
+    this.hint = function() {
+      //todo
+      console.log('hint not implemented.');
+    }
+
+    this.turn = function() {
+
+      // invoke game to determine state.
       let res = game.playersGuessSubmission(+this.guess());
 
+      // clear input and update past guesses
       this.guess("");
       let n  = game.pastGuesses.length
       ,   gs = this.guesses()
       gs[n-1] = game.pastGuesses[n-1];
       this.guesses(gs);
 
+      // Game over?
       if (res === sWin || res === sLose) {
         this.title(ResourceString[res])
         this.subtitle(ResourceString[sPlayAgain])
         return;
       }
 
+      // Validation concern from state
       if (res === sGuessAgain) {
         this.subtitle(ResourceString[res]);
         return;
       }
 
+      // Update rest of visual components.
       let subtitle =  game.isLower() ? 'Go higher.' : 'Go lower.'
       this.subtitle(subtitle);
       this.title(ResourceString[res]);
