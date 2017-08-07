e2cdd381ca2b9fefccbb0cd109bcade7a931aa86
Amal Sudama <cds.sudama@gmail.com>
2017-08-07 06:40:17 -0400

    wip

---
+++ GuessingGame.js
@@ -0,0 +1,87 @@
+const sBurn       = Symbol('Burning up')
+,     sLukewarm   = Symbol('Lukewarm')
+,     sChilly     = Symbol('Chilly')
+,     sIceCold    = Symbol('IceCold')
+,     sLose       = Symbol('Lose')
+,     sWin        = Symbol('Win')
+,     sGuessAgain = Symbol('GuessAgain')
+,     sInvalid    = Symbol('Invalid')
+,     sPrompt     = Symbol('Prompt')
+
+const ResourceString = {
+  [sBurn      ] : "You're burning up!",
+  [sLukewarm  ] : "You're lukewarm.",
+  [sChilly    ] : "You're a bit chilly.",
+  [sIceCold   ] : "You're ice cold!",
+  [sWin       ] : "You Win!",
+  [sLose      ] : "You Lose.",
+  [sInvalid   ] : "That is an invalid guess.",
+  [sGuessAgain] : "You have already guessed that number.",
+  [sPrompt    ] : "Guess a number between 1-100!"
+}
+
+function generateWinningNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+function shuffle(A) {
+  let last = A.length
+  ,   i
+  ;
+
+  while (last !== 0) {
+    // choose randomly & swap
+    i = Math.floor(Math.random() * last--);
+    [A[last], A[i]] = [A[i], A[last]];
+  }
+  return A;
+}
+
+function Game() {
+  this.playersGuess  = null;
+  this.pastGuesses   = [];
+  this.winningNumber = generateWinningNumber();
+  this.over          = false;
+}
+
+Game.prototype.difference = function() {
+  return Math.abs(this.winningNumber - this.playersGuess);
+}
+
+Game.prototype.isLower = function() {
+  return this.playersGuess < this.winningNumber;
+}
+
+Game.prototype.checkGuess = function() {
+  let offset = this.difference()
+  ,   dupGuess = this.pastGuesses.indexOf(this.playersGuess) !== -1
+  ,   nGuesses
+  ;
+
+  if (!dupGuess) this.pastGuesses.push(this.playersGuess);
+  nGuesses = this.pastGuesses.length;
+
+  let msg = !offset     ? sWin
+       : nGuesses === 5 ? sLose
+       : dupGuess       ? sGuessAgain
+       : offset < 10    ? sBurn
+       : offset < 25    ? sLukewarm
+       : offset < 50    ? sChilly
+       : sIceCold
+       ;
+  this.over = (msg === sWin || msg === sLose);
+  return ResourceString[msg]
+}
+
+Game.prototype.playersGuessSubmission = function(guess) {
+  if (isNaN(guess) || guess < 1 || guess > 100) throw (ResourceString[sInvalid]);
+  this.playersGuess = guess;
+  return this.checkGuess();
+}
+
+Game.prototype.provideHint = function() {
+  return shuffle([0, 0].map(generateWinningNumber)
+                       .concat(this.winningNumber));
+}
+
+function newGame() { return new Game(); }
---
+++ GuessingGameController.js
@@ -0,0 +1,64 @@
+function handleTurn(game) {
+  let input    = $('#player-input')
+  ,   guess    = +input.val()
+  ,   feedback = game.playersGuessSubmission(guess)
+  ,   prompt   = ''
+  ,   cssKlass = null
+  ;
+
+  cssKlass = feedback === ResourceString[sBurn] ? 'burn'
+           : feedback === ResourceString[sIceCold] ? 'ice-cold'
+           : feedback === ResourceString[sLukewarm] ? 'lukewarm'
+           : null;
+  // render input & guesses.
+  input.val('');
+
+  game.pastGuesses.forEach((g, i) => {
+    let el = $(`ul#guess-list li:nth-child(${i+1})`);
+    el.text(g);
+    if (game.pastGuesses.length-1 === i && cssKlass) el.addClass(cssKlass);
+  });
+
+  if (feedback === ResourceString[sGuessAgain]) {
+    $('h3').text(feedback);
+    $('h1').text("Guess Again!");
+  } else if (feedback === ResourceString[sWin] ||
+             feedback === ResourceString[sLose]) {
+    $('input').prop( "disabled", true );
+    $('#submit').prop( "disabled", true );
+    $('#hint').prop( "disabled", true );
+    $('h1').text(feedback);
+    $('h3').text("Play Again?");
+  } else {
+    prompt = game.isLower() ? 'higher' : 'lower';
+    prompt = `${feedback} Try ${prompt}.`
+    $('h3').text(prompt);
+  }
+}
+
+$(document).ready(function() {
+  let game = new Game();
+
+  $('#reset').on('click', function(event){
+    $('#player-input').val('');
+    game.pastGuesses.forEach((_, i) => {
+      let el = $(`ul#guess-list li:nth-child(${i+1})`);
+      el.text('-').removeClass('ice-cold lukewarm burn');
+    });
+    game = new Game();
+    $('h3').text(ResourceString[sPrompt]);
+    $('input').prop( "disabled", false );
+    $('#submit').prop( "disabled", false );
+    $('#hint').prop( "disabled", false );
+  });
+
+  $('#submit').on('click', function(event){
+    handleTurn(game);
+  });
+
+  $('#player-input').on('keypress', function(event) {
+    if (event.key === 'Enter') {
+      handleTurn(game);
+    }
+  });
+});
